b70615e1a8cf97a10b79c63d7e2c8bfd2e0f4b2c
gribasads <80699431+gribasads@users.noreply.github.com>
2022-05-01 03:31:27 -0300

    created table component

--- pages/historic.tsx
+++ pages/historic.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import Table from '../components/table'
 import Page from '../components/template/Page'
 
 export default function historic() {
+
+  const headers = [
+    { key: 'id', label: 'ID' },
+    { key: 'name', label: 'Name' },
+  ]
+  const data = [
+    { id: 1, name: 'John Doe', email: 'gmail.com' },
+    { id: 2, name: 'John Doe', email: 'gmail.com' },
+    { id: 3, name: 'John Doe', email: 'gmail.com' },
+  ]
+
   return (
-    <Page title='Historico'><h1>Historico</h1></Page>
+    <Page title='Histórico'>
+      <Table data={data} headers={headers}/> 
+    </Page>
   )
 }
