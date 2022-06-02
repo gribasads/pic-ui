37866ca94c0e01e489603ade61c4b08a25311828
gribasads <80699431+gribasads@users.noreply.github.com>
2022-06-01 23:24:05 -0300

    added disabled effect

--- components/Button/confirm.tsx
+++ components/Button/confirm.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 interface IProps {
   name: string
@@ -7,6 +7,9 @@ interface IProps {
 }
 export default function ConfirmButton(props: IProps) {
   const { name, func, disabled } = props
+  
+ 
+
   return (
     <button className={`p-2 bg-black border rounded 
      border-gray-500 hover:opacity-70 text-white ${disabled == true ? 'opacity-70 cursor-not-allowed':null}`} disabled={disabled} onClick={func}>{name}</button>
