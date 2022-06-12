9dca88f57cbd24044507c58f6a42f36febaf7b06
gribasads <80699431+gribasads@users.noreply.github.com>
2022-06-12 15:16:46 -0300

    fix backButton

--- components/Button/back.tsx
+++ components/Button/back.tsx
@@ -5,6 +5,6 @@ export default function BackButton() {
   const router = useRouter()
   return (
     <button className='p-2 bg-white border-2 rounded 
-    border-solid border-gray-500 hover:opacity-70' onClick={()=>router.back()}>Voltar</button>
+    border-solid border-gray-500 hover:opacity-70' onClick={()=>router.push('/home')}>Voltar</button>
   )
 }
