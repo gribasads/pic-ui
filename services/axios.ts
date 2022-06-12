49582ca2d46d1c95bb608d55c1a8da539d2c3a2b
gribasads <80699431+gribasads@users.noreply.github.com>
2022-06-12 00:56:11 -0300

    added token send

--- services/axios.ts
+++ services/axios.ts
@@ -2,15 +2,15 @@ import axios from "axios";
 import { parseCookies } from "nookies";
 
 export function getAPIClient(ctx?: any) {
-  const { 'nextauthtoken': token } = parseCookies(ctx)
+  const { 'token': token } = parseCookies(ctx)
 
   const api = axios.create({
     baseURL: 'http://localhost:3030',
   })
 
-//   if (token) {
-//     api.defaults.headers['Authorization'] = `Bearer ${token}`;
-//   }
+   if (token) {
+     api.defaults.headers['x-access'] = `${token}`;
+   }
 
   return api;
 }
